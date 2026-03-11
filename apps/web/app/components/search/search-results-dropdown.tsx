23c83170ef4f20232162bda1bdaff766d2b44e86
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-11 21:22:04 +0530

    feat(search): add search results dropdown component

---
+++ apps/web/app/components/search/search-results-dropdown.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export function SearchResultsDropdown() {
+  return (
+    <div className="absolute mt-2 w-96 bg-white border rounded-lg shadow-lg">
+      <div className="p-3 text-sm text-gray-500">Start typing to search...</div>
+    </div>
+  );
+}
