1e1f56b50bb1d25a35ca6324f5f14574e7eeb333
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-11 14:00:19 +0530

    feat: fetch organizations api, render organizations in dropdown

--- apps/web/app/components/org/org-switcher.tsx
+++ apps/web/app/components/org/org-switcher.tsx
@@ -1,11 +1,37 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { useAuth } from '@/context/auth-context';
+import { OrgItem } from './org-item';
+
+type Org = {
+  id: string;
+  name: string;
+};
+
 export function OrgSwitcher() {
+  const { token } = useAuth();
+  const [orgs, setOrgs] = useState<Org[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/org', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setOrgs(data.data));
+  }, []);
+
   return (
     <select className="border rounded-md px-3 py-2 w-[200px]">
       <option>Select Workspace</option>
-      <option>Workspace 1</option>
-      <option>Workspace 2</option>
+
+      {orgs
+        ? orgs.map((org: Org) => (
+            <OrgItem key={org.id} id={org.id} name={org.name} />
+          ))
+        : null}
     </select>
   );
 }
