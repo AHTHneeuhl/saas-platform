96e17142c4d76a21be155ed90369df32f4883431
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-11 10:30:44 +0530

    feat: Emit real-time events for notifications. 🚀

--- apps/api/src/notifications/notifications.service.ts
+++ apps/api/src/notifications/notifications.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { RealtimeGateway } from 'src/realtime/realtime.gateway';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class NotificationsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private realtimeGateway: RealtimeGateway,
+  ) {}
 
   async createNotification(userId: string, title: string, message: string) {
-    return this.prisma.notification.create({
+    const notification = await this.prisma.notification.create({
       data: {
         userId,
         title,
         message,
       },
     });
+
+    // realtime event
+    this.realtimeGateway.emit('notification.created', notification);
+
+    return notification;
   }
 
   async getUserNotifications(userId: string) {
