c1fbac6822e197957941567a417285fed1428ad5
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 15:43:03 +0530

    fix: register notifications queue in notifications module

--- apps/api/src/notifications/notifications.module.ts
+++ apps/api/src/notifications/notifications.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { NotificationsService } from './notifications.service';
-import { NotificationsController } from './notifications.controller';
 import { RealtimeModule } from 'src/realtime/realtime.module';
 
+import { NotificationsController } from './notifications.controller';
+import { NotificationsService } from './notifications.service';
+
 @Module({
-  imports: [PrismaModule, RealtimeModule],
+  imports: [
+    PrismaModule,
+    RealtimeModule,
+    BullModule.registerQueue({
+      name: 'notifications',
+    }),
+  ],
   providers: [NotificationsService],
   controllers: [NotificationsController],
   exports: [NotificationsService],
