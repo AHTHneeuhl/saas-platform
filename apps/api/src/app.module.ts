0e64752ce5f4b27f466059c1d72cf0ff017e984e
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 15:19:03 +0530

    fix: update throttler configuration for latest api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { ActivityLogsModule } from './activity-logs/activity-logs.module';
 import { AuthModule } from './auth/auth.module';
 import { CommentsModule } from './comments/comments.module';
 import { LabelsModule } from './labels/labels.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { PrismaService } from './prisma/prisma.service';
 import { ProjectsModule } from './projects/projects.module';
-import { RealtimeGateway } from './realtime/realtime.gateway';
 import { SearchModule } from './search/search.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AttachmentsModule } from './attachments/attachments.module';
@@ -17,6 +18,15 @@ import { InfraModule } from './infra/infra.module';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60,
+          limit: 100,
+        },
+      ],
+    }),
+
     AuthModule,
     PrismaModule,
     ProjectsModule,
@@ -30,6 +40,6 @@ import { InfraModule } from './infra/infra.module';
     InfraModule,
   ],
   controllers: [AppController],
-  providers: [AppService, PrismaService, RealtimeGateway],
+  providers: [AppService],
 })
 export class AppModule {}
